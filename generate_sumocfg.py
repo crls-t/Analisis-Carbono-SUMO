4344039aa8aa1740e1e004036171ce930d566c29
crls-t <fghf358@gmail.com>
2024-07-25 15:43:06 -0500

    Actualización de tiempos sumocfg

--- generate_sumocfg.py
+++ generate_sumocfg.py
@@ -1,16 +1,16 @@
 import sys
 
-def generate_edgeData(edge,output_file2):
+def generate_edgeData(edge,output_file2,time_difference):
 	config_content= f"""<?xml version="1.0" encoding="UTF-8"?>
 
 <additional>
-    <edgeData id="1" period="3600" type="emissions" file="{edge}" excludeEmpty="true" />
+    <edgeData id="1" period="{time_difference}" type="emissions" file="{edge}" excludeEmpty="true" />
 </additional>
 """
 	with open(output_file2, 'w') as file:
 		file.write(config_content)
 
-def generate_sumocfg(route_file, additional_files, output_file,out):
+def generate_sumocfg(route_file, additional_files, output_file,out,time_difference):
     config_content = f"""<?xml version="1.0" encoding="UTF-8"?>
 
 <configuration>
@@ -19,6 +19,15 @@ def generate_sumocfg(route_file, additional_files, output_file,out):
         <route-files value="{route_file}"/>
         <additional-files value="{additional_files}"/>
     </input>
+
+<rerouting>
+	<!-- Probabilidad de que un vehículo tenga un dispositivo de redireccionamiento -->
+        <device.rerouting.probability value="0.5"/> 
+	<!-- Período en el que se desviará el vehículo -->
+        <device.rerouting.period value="10"/>
+        <!-- Enrutamiento paralelizado -->
+        <device.rerouting.threads value= "10"/>
+    </rerouting>
     
     <output>
         <tripinfo-output value="reportes_{out}/tripinfo.xml"/>
@@ -27,8 +36,10 @@ def generate_sumocfg(route_file, additional_files, output_file,out):
     </output>
 
     <time>
-        <end value="3600"/>
+        <end value="{time_difference}"/>
     </time>
+    
+
 </configuration>
 """
 
@@ -36,8 +47,8 @@ def generate_sumocfg(route_file, additional_files, output_file,out):
         file.write(config_content)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 7:
-        print("Uso: python generate_sumocfg.py <ruta_del_archivo_de_rutas> <archivos_adicionales> <archivo_de_salida>")
+    if len(sys.argv) != 9:
+        print("Uso: python generate_sumocfg.py <archivo_de_rutas> <archivos_adicionales> <archivo_de_salida> <carpeta_salida_reportes> <archivo_edgedata.xml> <archivo_salida_edgedata> <tiempo_inicio> <tiempo_final>" )
         sys.exit(1)
 
     route_file = sys.argv[1]
@@ -46,10 +57,26 @@ if __name__ == '__main__':
     out = sys.argv[4]
     output_file2= sys.argv[5]
     edge=sys.argv[6]
+    from_time=sys.argv[7]
+    to_time=sys.argv[8]
     
+    from_time_float = float(from_time)
+    to_time_float = float(to_time)
 
-    generate_sumocfg(route_file, additional_files, output_file,out)
+
+    # Calculate the time difference
+    if from_time_float == to_time_float:
+        time_difference = 24.0
+    elif to_time_float < from_time_float:
+        to_time_float += 24
+        time_difference = to_time_float - from_time_float
+    else:
+        time_difference = to_time_float - from_time_float
     
-    generate_edgeData(edge,output_file2)
+    time_difference=int(time_difference*3600)
+
+    generate_sumocfg(route_file, additional_files, output_file,out,time_difference)
+    
+    generate_edgeData(edge,output_file2,time_difference)
     print(f"Archivo {output_file} generado correctamente.")
 
